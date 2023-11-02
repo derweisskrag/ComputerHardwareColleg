f86606e82581718b880ea41621fcea3ddff389d6
Sergei <kompasj@mail.ru>
2023-11-02 08:17:39 +0200

    Loaded new document

--- ComputerHardware/Daemons/watchdog/daemon.py
+++ ComputerHardware/Daemons/watchdog/daemon.py
@@ -1,1 +1,44 @@
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
+from subprocess import run
+
+WATCH_PATH = "C:\\Users\\HomePC\\Desktop\\ComputerHardwareColleg\\ComputerHardware\\Daemons\\watchdog"
+
+class Monitor(FileSystemEventHandler):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+
+	def on_modified(self, event):
+		print(f"File {event.src_path} has been modified")
+		if event.src_path.endswith("main.py"):
+			run(["py", "main.py"])
+
+
+# Create an observer
+observer = Observer()
+
+# Register our event 
+event_handler = Monitor()
+
+# Schedule the event
+observer.schedule(
+	event_handler, 
+	path=WATCH_PATH, 
+	recursive=False)
+
+# start the observer
+observer.start()
+
+# Handler the process and stop it
+try:
+	print("The watchdog is watching...")
+	while True:
+		pass
+except KeyboardInterrupt:
+	observer.stop() 
+
+
+# Join the process
+observer.join()
---
+++ ComputerHardware/Daemons/watchdog/main.py
@@ -0,0 +1,15 @@
+def add(a: int, b: int) -> int:
+	return a + b
+
+
+
+def main():
+	num1 = 18
+	num2 = 24
+	result = add(num1, num2)
+
+	print(result)
+
+
+if __name__ == "__main__":
+	main()
